f22670ff34d33bd1db047e86bd5079ae7cbfc677
KunalSingh2001 <kunalsingh787711@gmail.com>
2025-04-16 18:07:51 +0530

    Initial commit with two folders

---
+++ 02vitereact/vite-project/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  let [counter, setCounter] = useState(0)
+  const addVal = () => {
+    console.log('clicked', counter);
+    if (counter < 20) {
+      setCounter (counter + 1);
+    }
+  }
+
+  const decreseVal = () => {
+    if (counter > 0) {
+      setCounter (counter - 1);
+    }
+  }
+
+  return (
+    <>
+    <h1>Hello</h1>
+    <h2>Counter Val: {counter}</h2>
+    <button onClick={addVal}>Add Value</button>
+    <button onClick={decreseVal}>Decrese Value</button>
+    </>
+  )
+}
+
+export default App
---
+++ 03taiwildProps/src/components/Card.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+function Card ({username}) {
+    console.log('props',username);
+    return (
+        <div>Card from card file</div>
+    )
+}
+export default Card;
